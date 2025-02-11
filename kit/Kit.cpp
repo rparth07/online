bc6579776fec33bc093497630fd875eb2b84c312
Jan Holesovsky <kendy@collabora.com>
2025-02-11 14:25:36 +0100

    coda-m: Disable the COOLWSD main() not to conflict
    Introduce ENABLE_CODA for that.
    
    Signed-off-by: Jan Holesovsky <kendy@collabora.com>
    Change-Id: I9752e52d1fd03629e9e4c02f7f8275550ac1f86a

--- kit/Kit.cpp
+++ kit/Kit.cpp
@@ -2905,6 +2905,7 @@ static void addRecording(const std::string &recording, bool force)
     traceEventRecords[force ? 0 : 1].push_back(recording + "\n");
 }
 
+#if !ENABLE_CODA // ie. normal server
 void TraceEvent::emitOneRecordingIfEnabled(const std::string &recording)
 {
     addRecording(recording, true);
@@ -2914,6 +2915,7 @@ void TraceEvent::emitOneRecording(const std::string &recording)
 {
     addRecording(recording, false);
 }
+#endif // !ENABLE_CODA
 
 #else
 
